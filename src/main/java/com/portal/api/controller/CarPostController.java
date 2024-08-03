1b48deffb8a93a50138c37ced2999461bbbbe965
marlei <marlei.melchior.prestador@prf.gov.br>
2024-08-03 18:32:34 -0300

    criação dos endpoint do RESTAPI de CARPOSTS

---
+++ src/main/java/com/portal/api/controller/CarPostController.java
@@ -0,0 +1,35 @@
+package com.portal.api.controller;
+
+import com.portal.api.dto.CarPostDTO;
+import com.portal.api.service.CarPostStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarPostController {
+
+    @Autowired
+    private CarPostStoreService carPostStoreService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<CarPostDTO>> getCarSales() {
+        return ResponseEntity.status(HttpStatus.FOUND).body(carPostStoreService.getCarsForSale());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id) {
+        carPostStoreService.changeCarForSale(carPostDTO, id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCarForSale(@PathVariable("id") String id) {
+        carPostStoreService.removeCarForSale(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
